3b7252460df996ee8aa1342e86810bfede1109c4
TANISHKA-RIYA <106598143+TANISHKA-RIYA@users.noreply.github.com>
2023-01-26 16:19:35 +0530

    p 9 2

--- quizgamepage.js
+++ quizgamepage.js
@@ -12,3 +12,32 @@ function send (){
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 }
+
+question_turn = localStorage.getItem('player1');
+answer_turn = localStorage.getItem('player2');
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value ;
+
+    if (get_answer == actual_answer){
+        if(answer_turn == "player1"){
+           update_player1_score = player1_score + 1;
+           document.getElementById("player1_score").innerHTML ="ANSWER TURN :"+ update_player1_score;
+        }else {
+            update_player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML ="ANSWER TURN :"+player2_score;
+        }
+    }
+
+    if (get_answer == actual_answer){
+        if(question_turn == "player1"){
+           update_player1_score = player1_score + 1;
+           document.getElementById("player1_score").innerHTML ="QUESTION TURN :"+update_player1_score;
+        }else {
+            update_player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML ="QUESTION TURN :"+player2_score;
+        }
+    }
+
+    document.getElementById("output").innerHTML = "";
+}
